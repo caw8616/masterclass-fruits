960299ace68f22cde23c1e580e0d1109b67ba8f0
Catherine Walters <caw8616@g.rit.edu>
2021-02-24 15:35:42 -0500

    Display Onboarding Screen

---
+++ Fructus/Fructus/App/FructusApp.swift
@@ -0,0 +1,23 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Catherine Walters on 2/24/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+struct FructusApp_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
