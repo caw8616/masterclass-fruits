b517a0df896af510d2f05020d122fdf72b914f71
Catherine Walters <caw8616@g.rit.edu>
2021-02-25 20:36:29 -0500

    Detail view more

--- Africa/Africa/View/AnimalDetailView.swift
+++ Africa/Africa/View/AnimalDetailView.swift
@@ -18,8 +18,40 @@ struct AnimalDetailView: View {
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
             VStack(alignment: .center, spacing: 20) {
-                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                // HERO IMAGE
+                Image(animal.image)
+                    .resizable()
+                    .scaledToFit()
+                
+                // TITLE
+                Text(animal.name.uppercased())
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .multilineTextAlignment(.center)
+                    .padding(.vertical, 8)
+                    .foregroundColor(.primary)
+                    .background(
+                        Color.accentColor
+                            .frame(height:6)
+                            .offset(y: 24)
+                    )
+                
+                // HEADLINE
+                Text(animal.headline)
+                    .font
+                
+                // GALLERY
+                
+                
+                // FACTS
+                
+                // DESCRIPTION
+                
+                // MAP
+                
+                // LINK
             } //: VSTACK
+            .navigationBarTitle("Learn about \(animal.name)", displayMode: .inline)
         } //: SCROLL
     }
 }
@@ -29,6 +61,9 @@ struct AnimalDetailView: View {
 struct AnimalDetailView_Previews: PreviewProvider {
     static let animals: [Animal] = Bundle.main.decode("animals.json")
     static var previews: some View {
-        AnimalDetailView(animal: animals[0])
+        NavigationView {
+            AnimalDetailView(animal: animals[0])
+        }
+        .previewDevice("iPhone 11 Pro")
     }
 }
